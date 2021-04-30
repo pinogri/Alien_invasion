131b2f22e5a9466e49bb87f3f356bb2d3d8e59ae
Administrator <admin@example.com>
2021-04-30 16:19:36 +0300

    Alien_invasion v. 1.1 add limits for ship by screen size

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,12 +8,12 @@ class AlienInvasion:
     """Class for control resource and game"""
 
     def __init__(self):
-        """Initializated game and create game resources"""
+        """Initialized game and create game resources"""
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_heigth))
         pygame.display.set_caption("Alien Invasion")
-        # Imort ship from class Ship
+        # Import ship from class Ship
         self.ship = Ship(self)
         # Set screen background color
 
@@ -21,14 +21,39 @@ class AlienInvasion:
         """Run main game cycle """
         while True:
             # Track events from keyboard and mouse
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            # At each pass of cycle set background color
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Display last screen
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                if event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                if event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                if event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                if event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
+
+    # At each pass of cycle set background color
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Display last screen
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
